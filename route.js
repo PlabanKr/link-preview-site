305336b34a5268de0ed554eaec83efdf08a880d9
Plaban Kumar Mondal <plaban.kr.mondal00@gmail.com>
2022-08-05 19:44:05 +0530

    Add link preview

--- route.js
+++ route.js
@@ -1,15 +1,16 @@
 const express = require("express");
 const router = express.Router();
-const { getLinkPreview, getPreviewFromContent } = require("link-preview-js");
+const { getLinkPreview } = require("link-preview-js");
 
 
 router.get("/", (req, res) => {
     res.render("home");
 });
 
-router.post("/get-data", (req, res) => {
+router.post("/get-data", async (req, res) => {
     const { link } = req.body;
-    res.send(link);
+    const preview = await getLinkPreview(link);
+    res.send(preview);
 });
 
 module.exports = router;
